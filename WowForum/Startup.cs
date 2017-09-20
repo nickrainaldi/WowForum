d0b7ceb97218d108b24dbd7d51829887968851e4
nickrainaldi <nickrainaldi@gmail.com>
2017-09-19 20:21:37 -0400

    first commit

---
+++ WowForum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WowForum.Startup))]
+namespace WowForum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
